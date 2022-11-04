8efcde954cc8b4230a6f42f128e8026f3ca10776
Alex Merced <alex@alexmerced.dev>
2022-11-03 20:38:52 -0400

    basic setup

--- readme.md
+++ readme.md
@@ -2,6 +2,14 @@
 
 This repository has an express app at many different states to model the setup for different situations. Explore the branches and checkout this readme to learn about each state.
 
-## Main Branch
+## basic-express-setup Branch
 
-This branch is an empty project with just your basic `.gitignore` file to have git ignore committing certain files.
+This branch is a basic express app with no templating engine or database libraries. Basic general use middleware libraries are installed.
+
+checkout comments for more details
+
+Any variables like `process.env.PORT` are assumed to have been defined in a .env file like so...
+
+```
+PORT=4000
+```
---
+++ server.js
@@ -0,0 +1,42 @@
+//**************************
+// DEPENDENCIES
+//**************************
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+const cors = require("cors")
+
+//**************************
+// GLOBAL VARIABLES
+//**************************
+const PORT = process.env.PORT || 3000
+
+//**************************
+// Create Express App Object
+//**************************
+const app = express()
+
+//**************************
+// Register Middleware
+//**************************
+app.use(morgan("tiny")) // log every request
+app.use(cors()) // allow cross-site requests
+app.use("/static", express.static("public")) // "/static" urls will serve files from public folder
+app.use(methodOverride("_method")) // enable overriding form submission methods
+app.use(express.urlencoded({extended: true})) // parse urlencoded bodies
+app.use(express.json()) // parse JSON bodies
+
+//**************************
+// Register Routes
+//**************************
+// main route
+app.use("/", (req, res) => {
+    res.send("Hello World")
+})
+
+//**************************
+// Server Listener
+//**************************
+app.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`)
+})
